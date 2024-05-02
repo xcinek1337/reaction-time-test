8355d89ada78dbe045a84f440cd1f00d2c2b412f
Marcin-laptok <marcinchodkowski004@wp.pl>
2024-05-02 18:48:15 +0200

    optimized spacing

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,54 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 export const Game = () => {
-	const [startTime, setStartTime] = useState(0);
+	const [startTime, setStartTime] = useState(0)
 
-	const [countReaction, setCountReaction] = useState(false);
-	const [isResultReady, setIsResultReady] = useState(false);
-	const [result, setResult] = useState(0);
+	const [countReaction, setCountReaction] = useState(false)
+	const [isResultReady, setIsResultReady] = useState(false)
+	const [result, setResult] = useState(0)
 
-	const randomTime = Math.ceil(Math.random() * 5000) + 1000;
+	const randomTime = Math.ceil(Math.random() * 5000) + 1000
 
 	useEffect(() => {
 		setTimeout(() => {
-			setCountReaction(true);
-			setStartTime(new Date().getTime());
-		}, randomTime);
-	}, []);
+			setCountReaction(true)
+			setStartTime(new Date().getTime())
+		}, randomTime)
+	}, [])
 
 	const handleClick = () => {
-		if (isResultReady) return;
-		const endTime = new Date().getTime();
-		const result = endTime - startTime;
-		setResult(result);
-		setIsResultReady(true);
-	};
+		if (isResultReady) return
+		const endTime = new Date().getTime()
+		const result = endTime - startTime
+		setResult(result)
+		setIsResultReady(true)
+	}
 	const handleRestart = () => {
-		setCountReaction(false);
-		setIsResultReady(false);
-		setResult(0);
+		setCountReaction(false)
+		setIsResultReady(false)
+		setResult(0)
 		setTimeout(() => {
-			setCountReaction(true);
-			setStartTime(new Date().getTime());
-		}, randomTime);
-	};
+			setCountReaction(true)
+			setStartTime(new Date().getTime())
+		}, randomTime)
+	}
 	return (
 		<>
 			<div
 				onClick={handleClick}
-				className={`h-screen w-screen pt-10 text-center text-4xl bg-red-600 ${countReaction && 'bg-teal-900'}`}
-			>
+				className={`h-screen w-screen pt-10 text-center text-4xl bg-red-600 ${countReaction && 'bg-teal-900'}`}>
 				{!countReaction && <h1>FOCUS</h1>}
 				{countReaction && (
 					<>
 						<h1>click</h1>
 						{isResultReady && (
 							<div>
-								<p className='pt-80 text-7xl'>{result} Miliseconds</p>
-								<button
-									onClick={handleRestart}
-									className='mt-8 rounded-md border-4 border-black py-1.5 px-3'
-								>
+								<p className='pt-36 sm:pt-44 text-7xl'>{result} Miliseconds</p>
+								<button onClick={handleRestart} className='mt-8 rounded-md border-4 border-black py-1.5 px-3'>
 									try again
 								</button>
 							</div>
@@ -57,5 +53,5 @@ export const Game = () => {
 				)}
 			</div>
 		</>
-	);
-};
+	)
+}
